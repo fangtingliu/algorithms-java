c3f7154d58a1c437204db51814a9b4e814541468
Fangting Liu <fangting@wikibuy.com>
2016-11-10 23:03:38 -0600

    init week5

---
+++ week5/PointSET.java
@@ -0,0 +1,146 @@
+import java.util.TreeSet;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+    private TreeSet<Point2D> set;
+
+    public PointSET() {
+        set = new TreeSet<Point2D>();
+    }                              // construct an empty set of points
+
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    public int size() {
+        return set.size();
+    }
+
+    public void insert(Point2D p) {
+        if (p == null) throw new NullPointerException();
+        if (set.isEmpty() || !set.contains(p)) {
+            set.add(p);
+        }
+    }
+
+    public boolean contains(Point2D p) {
+        if (p == null) throw new NullPointerException();
+        return set.contains(p);
+    }
+
+    public void draw() {
+        StdDraw.clear();
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setXscale(-0.05, 1.05);
+        StdDraw.setYscale(-0.05, 1.05);   // leave a border to write text
+
+        for (Point2D p : set) {
+            p.draw();
+        }
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        System.out.println("rect" + rect);
+        if (rect == null) throw new NullPointerException();
+        TreeSet<Point2D> rangeSet = new TreeSet<Point2D>();
+        for (Point2D p : set) {
+            System.out.println("rect.contains(p)" + rect.contains(p) + p);
+            if (rect.contains(p)) {
+                rangeSet.add(p);
+            }
+        }
+        System.out.println("rangeSet" + rangeSet);
+        return rangeSet;
+
+    }             // all points that are inside the rectangle
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new NullPointerException();
+        double dis = Double.POSITIVE_INFINITY;
+        Point2D closest = null;
+        for (Point2D n : set) {
+            double currDis = p.distanceTo(n);
+            if (currDis < dis) {
+                closest = n;
+                dis = currDis;
+            }
+        }
+        return closest;
+    }
+
+    public static void main(String[] args) {
+        String filename = args[0];
+        In in = new In(filename);
+
+        StdDraw.enableDoubleBuffering();
+
+        // initialize the data structures with N points from standard input
+        PointSET brute = new PointSET();
+        while (!in.isEmpty()) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+            brute.insert(p);
+        }
+
+        double x0 = 0.0, y0 = 0.0;      // initial endpoint of rectangle
+        double x1 = 0.0, y1 = 0.0;      // current location of mouse
+        boolean isDragging = false;     // is the user dragging a rectangle
+
+        // draw the points
+        StdDraw.clear();
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        brute.draw();
+        StdDraw.show();
+
+        while (!false) {
+
+            // user starts to drag a rectangle
+            if (StdDraw.mousePressed() && !isDragging) {
+                x0 = StdDraw.mouseX();
+                y0 = StdDraw.mouseY();
+                isDragging = true;
+                continue;
+            }
+
+            // user is dragging a rectangle
+            else if (StdDraw.mousePressed() && isDragging) {
+                x1 = StdDraw.mouseX();
+                y1 = StdDraw.mouseY();
+                continue;
+            }
+
+            // mouse no longer pressed
+            else if (!StdDraw.mousePressed() && isDragging) {
+                isDragging = false;
+            }
+
+
+            RectHV rect = new RectHV(Math.min(x0, x1), Math.min(y0, y1),
+                    Math.max(x0, x1), Math.max(y0, y1));
+            // draw the points
+            StdDraw.clear();
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.setPenRadius(0.01);
+            brute.draw();
+
+            // draw the rectangle
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.setPenRadius();
+            rect.draw();
+
+            // draw the range search results for brute-force data structure in red
+            StdDraw.setPenRadius(0.03);
+            StdDraw.setPenColor(StdDraw.RED);
+            for (Point2D p : brute.range(rect))
+                p.draw();
+
+            StdDraw.show();
+            StdDraw.pause(40);
+        }
+    }
+}
